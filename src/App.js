f8bc88010440b0a53b9ac4f83b95a7f27b015e74
SyedaHaya <141368715+SyedaHaya@users.noreply.github.com>
2023-09-22 03:56:08 +0500

    my first react app

--- src/App.js
+++ src/App.js
@@ -9,15 +9,17 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
+        <div
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
-      </header>
+        </div>
+      </header> 
+      
+      
     </div>
   );
 }
